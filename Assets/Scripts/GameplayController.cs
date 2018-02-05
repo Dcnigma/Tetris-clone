f9894f7a13d2dee662895d4f244980e0c86ebd0b
Dcnigma <bart_is@hotmail.com>
2018-02-05 11:15:42 +0100

    Initial commit

---
+++ Assets/Scripts/GameplayController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameplayController : MonoBehaviour {
+	[SerializeField]
+
+	private GameObject pausePanel;
+
+void awake()
+	{
+		pausePanel.SetActive (false);
+	}
+
+
+
+	// Use this for initialization
+
+	public void PauseGame () {
+		Time.timeScale = 0f;
+		pausePanel.SetActive (true);
+
+	}
+	
+	// Update is called once per frame
+
+	public void ResumeGame () {
+		Time.timeScale = 1f;
+		pausePanel.SetActive (false);
+
+	}
+	public void MainMenu () {
+		Time.timeScale = 1f;
+		SceneManager.LoadScene ("Menu");
+
+	}
+
+}
---
+++ Assets/Scripts/Menu Controller/mainmenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class mainmenu : MonoBehaviour {
+
+
+	public void PlayGame ()
+	{
+		SceneManager.LoadScene ("MainGame");
+
+	}
+
+	public void Options ()
+	{
+		SceneManager.LoadScene ("Options");
+
+	}
+
+	public void Options_Back ()
+	{
+		SceneManager.LoadScene ("Menu");
+
+	}
+}
